c26a48cd99a943d0c220a8cc94f5c121c831e5a8
deboracorrales <deboramailen88@gmail.com>
2021-04-10 15:56:42 -0300

    Initial commit

---
+++ Guia_ejercicio02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia_ejercicio02
+//Ej 02: Solicite el ingreso de un texto cualquiera y lo presente nuevamente al usuario.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+
+            Console.WriteLine("Ingrese una frase.");
+            texto = Console.ReadLine();
+            Console.WriteLine("Usted ingreso: " + texto);
+            
+            Console.ReadKey();
+            //est es una prueba
+        }
+    }
+}
